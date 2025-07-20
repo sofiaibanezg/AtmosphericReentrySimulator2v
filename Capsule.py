d51e412a13661219e86821d5fcf607663d9df6f0
Sofia Ibanez <sofia.ibanez@udea.edu.co>
2025-07-20 11:51:54 -0500

    Capsule creation

---
+++ Capsule.py
@@ -0,0 +1,16 @@
+# src/capsule.py
+
+class Capsule:
+    def __init__(self, mass, area, drag_coefficient):
+        self.mass = mass  # kg
+        self.area = area  # m^2
+        self.drag_coefficient = drag_coefficient
+        self.altitude = 120000  # m
+        self.velocity = -100    # m/s (negativo = descendiendo)
+        self.temperature = 300  # K
+
+    def is_landed(self):
+        return self.altitude <= 0
+
+    def __str__(self):
+        return f"Altitud: {self.altitude} m | Velocidad: {self.velocity} m/s | Temp: {self.temperature} K"
