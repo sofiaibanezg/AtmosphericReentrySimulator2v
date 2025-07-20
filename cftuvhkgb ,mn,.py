076f0e07fa9a213126e147097d644339a5dd9a9c
Sofia Ibanez <sofia.ibanez@udea.edu.co>
2025-07-20 11:53:01 -0500

    Deleted files

--- cftuvhkgb ,mn,.py
+++
@@ -1,8 +0,0 @@
-class Capsule:
-    def __init__(self, mass, area, drag_coefficient):
-        self.mass = mass  # kg
-        self.area = area  # m^2
-        self.drag_coefficient = drag_coefficient
-        self.altitude = 120000  # m (inicia en 120 km)
-        self.velocity = -100  # m/s (negativo = bajando)
-        self.temperature = 300  # K
